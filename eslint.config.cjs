52019f5c11bd8ec2255ffe7efdfa263b4e3baf40
Dmitriy <babygreenyoda@yandex.ru>
2026-03-09 00:40:32 +0800

    Initial deploy configurations

---
+++ eslint.config.cjs
@@ -0,0 +1,33 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**', 'coverage/**', 'eslint.config.cjs'],
+  },
+  ...compat.env({
+    browser: true,
+    node: true,
+    es2024: true,
+  }),
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ),
+  ...compat.config({
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: ['./tsconfig.json'],
+    },
+  }),
+];
